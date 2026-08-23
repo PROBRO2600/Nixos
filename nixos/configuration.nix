53f2731d54bc55abe4ce21d4c60747de957b2e60
NixOS Backup Daemon <backup-daemon@localhost>
2026-08-23 17:52:26 +0530

    Automated backup: 2026-08-23 17:52:26

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -24,7 +24,7 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
+  
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -121,21 +121,10 @@ hardware.bluetooth = {
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_IN";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_IN";
-    LC_IDENTIFICATION = "en_IN";
-    LC_MEASUREMENT = "en_IN";
-    LC_MONETARY = "en_IN";
-    LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
-    LC_PAPER = "en_IN";
-    LC_TELEPHONE = "en_IN";
-    LC_TIME = "en_IN";
-  };
-
+# Select internationalisation properties.
+# Select internationalisation properties.
+i18n.defaultLocale = "en_US.UTF-8";
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = false;
